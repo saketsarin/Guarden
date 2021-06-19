970fd5a8d42150a130538f0eb18c56f77741d094
saketsarin <sarinsaket@gmail.com>
2021-06-20 01:43:23 +0530

    added pages

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
-$(document).ready(function () {
-  var multipleCancelButton = new Choices("#choices-multiple-remove-button", {
-    removeItemButton: true,
-    maxItemCount: 5,
-    searchResultLimit: 5,
-    renderChoiceLimit: 5,
-  });
-});
-
-
 var field = 1; //inital number of field in form
 
 function new_field() {
-
   field++;
-  var objTo = document.getElementById('insert_field');
-  var divtest = document.createElement('div');
-  divtest.setAttribute('class', 'form-group removeclass' + field);
-  var rdiv = 'removeclass' + field;
+  var objTo = document.getElementById("insert_field");
+  var divtest = document.createElement("div");
+  divtest.setAttribute("class", "form-group removeclass" + field);
+  var rdiv = "removeclass" + field;
   divtest.innerHTML = ` <div class="fields" style="display: flex">
   <div class="form-group">
       <div class="input-group">
@@ -39,11 +28,11 @@ function new_field() {
           <button class="btn btn-danger" type="button"  onclick="remove_field(${field});"><i class="fa fa-minus" aria-hidden="true" style="margin: 13px;"></i></button>
         </div>
 
-</div>`
+</div>`;
 
-  objTo.appendChild(divtest)
+  objTo.appendChild(divtest);
 }
 
-function remove_field (rid) {
-  $('.removeclass' + rid).remove();
+function remove_field(rid) {
+  $(".removeclass" + rid).remove();
 }
